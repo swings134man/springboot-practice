a6f8d0cc6db0f63cf8c347a8efd87ec829664b4c
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2022-08-03 01:44:27 +0900

    Java version 8 -> 11, property setting change

--- src/main/java/boot/bootprac/controller/HelloController.java
+++ src/main/java/boot/bootprac/controller/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model){
-        model.addAttribute("data", "hello!!!");
+        model.addAttribute("data", "hello!!!"); // model의 이름 data
         return "hello";
     }
 }
