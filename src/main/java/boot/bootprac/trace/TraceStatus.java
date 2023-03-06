d3de75caf22bd62094e68dab5ce0e8816b96aa99
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2023-03-06 18:37:41 +0900

    Log추적기 동작 클래스 & console Test

---
+++ src/main/java/boot/bootprac/trace/TraceStatus.java
@@ -0,0 +1,38 @@
+package boot.bootprac.trace;
+
+/************
+ * @info : 로그 추적기의 기본 데이터 파일 -> TraceStatus 로그의 상태정보를 나타내는 클래스
+ * @name : TraceStatus
+ * @date : 2023/03/06 5:56 PM
+ * @author : SeokJun Kang(swings134@gmail.com)
+ * @version : 1.0.0
+ * @Description : log의 시작하면, 끝이 필요한데 로그가 종료되었다는것을 표현하기 위한 상태 클래스.
+ *
+ * - TraceId : 내부에 트랜잭션ID를 포함.
+ * - startTimeMs : 로그 시작시간, 로그 종료시 이 시간을 기준으로 시작~종료까지 전체 수행시간 계산.
+ * - message : 시작시 사용한 메시지, 이후 로그 종료시에도 해당 메세지를 사용해서 출력.
+ ************/
+public class TraceStatus {
+
+    private TraceId traceId;
+    private Long startTimeMs;
+    private String message;
+
+    public TraceStatus(TraceId traceId, Long startTimeMs, String message) {
+        this.traceId = traceId;
+        this.startTimeMs = startTimeMs;
+        this.message = message;
+    }
+
+    public TraceId getTraceId() {
+        return traceId;
+    }
+
+    public Long getStartTimeMs() {
+        return startTimeMs;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/test/java/boot/bootprac/trace/trace_one/FirstTraceV1Test.java
@@ -0,0 +1,32 @@
+package boot.bootprac.trace.trace_one;
+
+import boot.bootprac.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/************
+ * @info : FirstTraceV1 Class의 작동을 Test
+ * @name : FirstTraceV1Test
+ * @date : 2023/03/06 6:26 PM
+ * @author : SeokJun Kang(swings134@gmail.com)
+ * @version : 1.0.0
+ * @Description : log 출력, 측정하는 클래스를 테스트 진행.
+ ************/
+@SpringBootTest
+class FirstTraceV1Test {
+
+    @Test
+    void begin_end() {
+        FirstTraceV1 trace = new FirstTraceV1();
+        TraceStatus status = trace.begin("hello"); // 시작
+        trace.end(status);
+    }
+
+    @Test
+    void begin_exception() {
+        FirstTraceV1 trace = new FirstTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.exception(status, new IllegalStateException());
+    }
+
+}
