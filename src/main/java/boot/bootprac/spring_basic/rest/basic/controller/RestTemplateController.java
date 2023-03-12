d8445e989220ca208e50f79f15f9ebabeaa53005
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2023-03-13 01:28:18 +0900

    Rest Template basic Code + Swagger add

--- src/main/java/boot/bootprac/spring_basic/rest/basic/controller/RestTemplateController.java
+++ src/main/java/boot/bootprac/spring_basic/rest/basic/controller/RestTemplateController.java
@@ -4,8 +4,8 @@ import boot.bootprac.spring_basic.rest.basic.dto.RestUserResponse;
 import boot.bootprac.spring_basic.rest.basic.service.RestTemplateService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /************
  * @info : Spring RestTemplate Controller
@@ -13,30 +13,57 @@ import org.springframework.web.bind.annotation.RestController;
  * @date : 2023/03/12 7:30 PM
  * @author : SeokJun Kang(swings134@gmail.com)
  * @version : 1.0.0
- * @Description :
+ * @Description : IntelliJ Project - tenis 프로젝트에 연결됨.
  ************/
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/api/rest/")
 public class RestTemplateController {
 
     private final RestTemplateService service;
 
 
     /**
-     * @info    : Rest Template Get
+     * @info    : Rest Template Get1
      * @name    : get
      * @date    : 2023/03/12 9:41 PM
      * @author  : SeokJun Kang(swings134@gmail.com)
      * @version : 1.0.0
      * @param   :
      * @return  :
-     * @Description : RestTemplate Get
+     * @Description : RestTemplate Get - 단순 호출
      */
-    @GetMapping("/rest/get")
-    public RestUserResponse get() {
-        return service.getRest();
+    @GetMapping("get1")
+    public String get() {
+        return service.get1();
     }
 
+    @GetMapping("name")
+    public String getTest2(@RequestParam String name) {
+        log.info("getTest2 호출! param={}", name);
+        String name1 = service.getName();
+        return "getTest2(name) = " + name1;
+    }
+
+    // get3
+    @GetMapping("path/{name}")
+    public String name(@PathVariable String name) {
+        log.info("getTest3 호출! param={}", name);
+        String name1 = service.get3Name();
+        return "getPathVa = " + name1;
+    }
+
+    @PostMapping("user")
+    public ResponseEntity<RestUserResponse> getUser() {
+        ResponseEntity<RestUserResponse> user = service.getUser();
+        return user;
+    }
+
+    @PostMapping("addHeder")
+    public ResponseEntity<RestUserResponse> addHeader() {
+        ResponseEntity<RestUserResponse> result = service.addHeader();
+        return result;
+    }
 
 }
--- src/main/java/boot/bootprac/spring_basic/rest/basic/service/RestTemplateService.java
+++ src/main/java/boot/bootprac/spring_basic/rest/basic/service/RestTemplateService.java
@@ -3,6 +3,7 @@ package boot.bootprac.spring_basic.rest.basic.service;
 import boot.bootprac.spring_basic.rest.basic.dto.RestUserResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -17,6 +18,17 @@ import java.net.URI;
  * @author : SeokJun Kang(swings134@gmail.com)
  * @version : 1.0.0
  * @Description :
+ *
+ *  - Header 추가, 핸들링할때는 -> exchange() 사용.
+ *  - getForObject ->
+ *  - getForEntity -> 상태코드 및 내가 받는 데이터 타입을 정확히 명시됨 -> 파악하기 쉽다. -> 타겟 서버에서 보내주는 타입을 정확히 모르는경우 사용해도됨. -> String Type 으로 return.
+ *      -> Header 및 상세 정보를 얻을 수 있음. -> 추천하는 return 타입임.
+ *
+ *  1. post
+ *      1. post - Request Entity 사용시, Http Body -> object -> object mapper -> json -> rest template -> http body json으로 전송.
+ *      2.
+ *
+ *  TODO : getForObject 관련 데이터 확인. generic Response 추가
  ************/
 @Service
 @RequiredArgsConstructor
@@ -43,8 +55,126 @@ public class RestTemplateService {
 
         ResponseEntity<RestUserResponse> forEntityResult = restTemplate.getForEntity(uri, RestUserResponse.class); // 결과.
         return forEntityResult.getBody();
+    } //test 용
+
+    // get1 - 파라미터 없음.
+    public String get1() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/get1")
+                .encode()
+                .build()
+                .toUri();
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> forEntity = restTemplate.getForEntity(uri, String.class);
+
+        log.info("return 값 상태코드 : {}", forEntity.getStatusCode());
+        log.info("return body : {}", forEntity.getBody());
+        return forEntity.getBody();
+    }//get1
+
+
+    // get2 - param=name(String)
+    public String getName() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/name")
+                .queryParam("name", "king")
+                .encode()
+                .build()
+                .toUri();
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> forEntity = restTemplate.getForEntity(uri, String.class);
+
+        log.info("return 값 상태코드 : {}", forEntity.getStatusCode());
+        log.info("return body : {}", forEntity.getBody());
+        return forEntity.getBody();
+    }//get2()
+
+
+    // get3 - Path {} -> name
+    public String get3Name() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/path/{name}")
+                .encode()
+                .build()
+                .expand("kingking3") // 복수의 값을 넣어야 할경우 , 을 추가하여 구분함.
+                .toUri();
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> forEntity = restTemplate.getForEntity(uri, String.class);
+
+        log.info("return 값 상태코드 : {}", forEntity.getStatusCode());
+        log.info("return body : {}", forEntity.getBody());
+
+        return forEntity.getBody();
+    }//get3()
+
+
+    // post1 - getUser
+    public ResponseEntity<RestUserResponse> getUser() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/user")
+                .queryParam("id", "test1")
+                .queryParam("name", "test11")
+                .encode()
+                .build()
+                .toUri();
+
+        RestUserResponse dto = new RestUserResponse();
+        dto.setId("dto1Id");
+        dto.setName("dto1Name");
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        // 내부적으로, uri, dto(RequestBody로 인식) 필요함
+        ResponseEntity<RestUserResponse> result = restTemplate.postForEntity(uri, dto, RestUserResponse.class);
+
+        log.info("return 값 상태코드 : {}", result.getStatusCode());
+        log.info("return body : {}", result.getBody());
+
+        return result;
+    }// post1 - getUser
+
+
+    // post - addHeader
+    public ResponseEntity<RestUserResponse> addHeader() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/addHeader")
+                .encode()
+                .build()
+                .toUri();
+
+        RestUserResponse dto = new RestUserResponse();
+        dto.setId("dto2Id");
+        dto.setName("dto2Name");
+
+        // 요청 - Request Entity
+        RequestEntity<RestUserResponse> requestEntity = RequestEntity
+                .post(uri)
+                .header("server-header", "HeaderName") //Header key= Header Name
+                .body(dto);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        // Header를 설정을 해줄때는 exchange를 사용. - Request Entity 에서 header,body,uri를 설정해 주었음.
+        ResponseEntity<RestUserResponse> exchange = restTemplate.exchange(requestEntity, RestUserResponse.class);
+
+        log.info("return 값 상태코드 : {}", exchange.getStatusCode());
+        log.info("return body : {}", exchange.getBody());
+
+        return exchange;
     }
 
-    public void name() {
-    }
-}
+
+
+
+
+
+
+}//class
