88b8beae75b5287b9d491a6f3c11dd531b8ccd75
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2024-01-02 23:03:13 +0900

    ObjectMapper - Test! Json to list,map

--- src/test/java/boot/bootprac/boot_test/ObjectMapperTest.java
+++ src/test/java/boot/bootprac/boot_test/ObjectMapperTest.java
@@ -2,8 +2,14 @@ package boot.bootprac.boot_test;
 
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
 
 public class ObjectMapperTest {
 
@@ -47,13 +53,14 @@ public class ObjectMapperTest {
     }
 
 
+    @DisplayName("Json To Java Obj")
     @Test
     void jsonToObj() throws JsonProcessingException {
         System.out.println("Json To Java Obj TEST START!!");
         System.out.println("-----------------------------");
 
         // given
-        String json = "{\"name\":\"seokjun\",\"age\":29}";
+        String json = "{\"name\":\"lucas\",\"age\":29}";
         this.objectMapper = new ObjectMapper();
 
         // when
@@ -65,7 +72,45 @@ public class ObjectMapperTest {
 
         System.out.println("member.name = " + member.getName());
         System.out.println("member.age = " + member.getAge());
+    }
 
+    @DisplayName("Json Array To Java List")
+    @Test
+    void jsonArrayToList() throws JsonProcessingException {
+        System.out.println("JsonArray To List TEST START!!");
+        System.out.println("-----------------------------");
+
+        // given
+        String json = "[{\"name\":\"hong\",\"age\":29},{\"name\":\"kim\",\"age\":30}]";
+        this.objectMapper = new ObjectMapper();
+
+        // when
+        List<Member> members = objectMapper.readValue(json, new TypeReference<>() {});
+
+        // then
+        System.out.println("members = " + members);
+        System.out.println("-----------------------------");
+
+        System.out.println("members index 0 = " + members.get(0));
+        Assertions.assertThat(members.get(0).getName()).isEqualTo("hong");
+    }
+
+    @DisplayName("Json To Java Map")
+    @Test
+    void jsonToMap() throws JsonProcessingException {
+        System.out.println("Json To Java Map TEST START!!");
+        System.out.println("-----------------------------");
+
+        // given
+        String json = "{\"name\":\"lucas\",\"age\":29}";
+        this.objectMapper = new ObjectMapper();
+
+        // when
+        Map<String, Object> map = objectMapper.readValue(json, new TypeReference<>() {});
+
+        // then
+        Assertions.assertThat(map.get("name")).isEqualTo("lucas");
+        System.out.println("map = " + map);
     }
 
 }
